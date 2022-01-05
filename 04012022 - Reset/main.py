da2650eea00a42487df51a12a63e6f763a91c33f
AlMarBla97 <alberto97zgz@gmail.com>
2022-01-05 09:31:39 +0100

    Modificacion para conflicto con Dis2 en Master

--- 04012022 - Reset/main.py
+++ 04012022 - Reset/main.py
@@ -9,3 +9,4 @@ time.sleep(4)
 pycom.heartbeat(True)
 #modifico en el ordenador y a ver si se me sube bien al github online.
 #modifico esto en la ramav1, y a ver que pasa
+#Meto esto en el master, a ver ha conflicto con diseñador.
