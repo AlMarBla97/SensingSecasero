f601da7ec1e782803f00bf1abf88d05370e0182d
AlMarBla97 <alberto97zgz@gmail.com>
2022-01-05 10:49:21 +0100

    Change name of 04012022 to Reset. Module works.

--- Reset module/main.py
+++ Reset module/main.py
@@ -2,10 +2,6 @@ import pycom
 import time
 
 pycom.heartbeat(False)
-pycom.rgbled(0xff0000)
-time.sleep(4)
 pycom.rgbled(0x00ff00)
-time.sleep(4)
-pycom.rgbled(0x0000ff)
 time.sleep(4)
 pycom.heartbeat(True)
