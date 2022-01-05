1dbbc146ce539ee3e1ffb4f670f11b2f3df33f8e
AlMarBla97 <alberto97zgz@gmail.com>
2022-01-05 10:31:28 +0100

    Punto de partida y fin de las modificaciones

--- 04012022 - Reset/main.py
+++ 04012022 - Reset/main.py
@@ -2,13 +2,6 @@ import pycom
 import time
 
 pycom.heartbeat(False)
-pycom.rgbled(0x999900)
-time.sleep(4)
 pycom.rgbled(0x00ff00)
 time.sleep(4)
 pycom.heartbeat(True)
-#modifico en el ordenador y a ver si se me sube bien al github online.
-#modifico esto en la ramav1, y a ver que pasa
-#Meto esto en el master, a ver ha conflicto con diseñador.
-#Esto lo meto desde Diseñador 2. 
-#Meto esto en el editor, para ver si me lo sube a Github.
